a57adc47933efc339a9a975d563408940319edfb
julia98m <juliamoussa98@gmail.com>
2026-09-01 22:15:18 +0300

    added better auth

---
+++ lib/actions/auth.ts
@@ -0,0 +1,74 @@
+"use server"
+
+import { headers } from "next/headers"
+import { auth } from "@/lib/auth"
+import {
+  actionError,
+  ActionResponse,
+  actionSuccess,
+} from "@/lib/action-response"
+
+type LoginResponse = {
+  username: string
+  token: string
+}
+
+export async function signInAction({
+  email,
+  password,
+}: {
+  email: string
+  password: string
+}): Promise<ActionResponse<LoginResponse>> {
+  try {
+    const { user, token } = await auth.api.signInEmail({
+      body: { email, password },
+      headers: await headers(),
+    })
+    return actionSuccess({ username: user.name, token }, "Log in success")
+  } catch (error) {
+    return actionError(error, "Log in error")
+  }
+}
+
+export async function signUpAction({
+  name,
+  email,
+  password,
+}: {
+  name: string
+  email: string
+  password: string
+}): Promise<ActionResponse<any>> {
+  try {
+    const { user } = await auth.api.signUpEmail({
+      body: { name, email, password },
+      headers: await headers(),
+    })
+    return actionSuccess(user, "Singup success")
+  } catch (error) {
+    return actionError(error, "Signup failed")
+  }
+}
+
+export async function signOutAction(): Promise<ActionResponse<any>> {
+  try {
+    await auth.api.signOut({
+      headers: await headers(),
+    })
+    return actionSuccess(null, "Signout success")
+  } catch (error) {
+    return actionError(error, "Signout failed")
+  }
+}
+
+export async function getSessionAction() {
+  try {
+    const session = await auth.api.getSession({
+      headers: await headers(),
+    })
+    return session
+  } catch {
+    return null
+  }
+}
---
+++ lib/auth.ts
@@ -0,0 +1,26 @@
+import { betterAuth } from "better-auth"
+import { prismaAdapter } from "better-auth/adapters/prisma"
+import { prisma } from "@/lib/prisma"
+import { nextCookies } from "better-auth/next-js"
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        defaultValue: "CLIENT",
+        input: false,
+      },
+    },
+  },
+  session: {
+    expiresIn: 60 * 60 * 24 * 7,
+  },
+  plugins: [nextCookies()],
+})
