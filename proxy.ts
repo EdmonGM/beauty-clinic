8fc5077b3da1f2ac9ac9f95b8706d27c48311d5a
julia98m <juliamoussa98@gmail.com>
2026-09-04 21:06:29 +0300

    protected admin routes

--- proxy.ts
+++ proxy.ts
@@ -1,23 +1,23 @@
 import { NextRequest, NextResponse } from "next/server"
-import { auth } from "./lib/auth"
-import { headers } from "next/headers"
+import { getCachedSession } from "./lib/auth-server-hooks"
 
 export async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  })
+  const session = await getCachedSession()
 
   if (!session) {
     return NextResponse.redirect(new URL("/login", request.url))
   }
+
   if (pathname.startsWith("/admin")) {
     if (session.user.role !== "ADMIN") {
-      return NextResponse.redirect(new URL("/unaithorized", request.url))
+      return NextResponse.redirect(new URL("/unauthorized", request.url))
     }
   }
-  return NextResponse.next()
+  const response = NextResponse.next()
+  response.headers.set("x-pathname", pathname)
+  return response
 }
 export const config = {
-  matcher: ["/"], // Specify the routes the middleware applies to
+  matcher: ["/admin/:path*"], // Specify the routes the middleware applies to
 }
