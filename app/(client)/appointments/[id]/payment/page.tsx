8b3c1c16957093e14f1dfd6df8ca73875a63964a
julia98m <juliamoussa98@gmail.com>
2026-09-24 11:36:41 +0300

    changed error message

--- app/(client)/appointments/[id]/payment/page.tsx
+++ app/(client)/appointments/[id]/payment/page.tsx
@@ -8,6 +8,8 @@ import { PaymentForm } from "@/components/client/payment-form"
 import { useParams } from "next/navigation"
 import { Spinner } from "@/components/ui/spinner"
 import { PageLoader } from "@/components/page-loader"
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
 
 const stripePromise = loadStripe(
   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
@@ -32,7 +34,17 @@ export default function PaymentPage() {
   }, [id])
 
   if (isPending) return <PageLoader />
-  if (!clientSecret) return <div>Failed to initialize payment</div>
+  if (!clientSecret)
+    return (
+      <div className="flex flex-col gap-2">
+        <p>Something went wrong! Try again later.</p>
+        <Button
+          nativeButton={false}
+          className="w-fit"
+          render={<Link href="/appointments">Go Back</Link>}
+        />
+      </div>
+    )
 
   return (
     <div className="mx-auto max-w-md py-12">
