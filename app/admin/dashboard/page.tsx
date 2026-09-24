30ccf32919740df48951f38604ffb92a0145b19e
julia98m <juliamoussa98@gmail.com>
2026-09-24 14:04:38 +0300

    added more text

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,3 +1,10 @@
 export default function DashboardPage() {
-  return <div>Dashboard</div>
+  return (
+    <div className="flex min-h-full flex-col items-center justify-center text-center">
+      <h1 className="mb-2 text-2xl font-bold">Admin Dashboard</h1>
+      <p className="text-muted-foreground">
+        Select an option from the sidebar to get started
+      </p>
+    </div>
+  )
 }
